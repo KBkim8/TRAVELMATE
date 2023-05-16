01de911a940936d11f6a63745ca2d25b840ef594
DESKTOP-PKLA6F4\user2 <>
2023-05-16 19:05:27 +0900

    0516com_1

--- TravelMate/src/main/java/com/kh/app/pay/controller/PayRoomController.java
+++ TravelMate/src/main/java/com/kh/app/pay/controller/PayRoomController.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/pay/room")
+@WebServlet("/reservation")
 public class PayRoomController extends HttpServlet{
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		req.getRequestDispatcher("/WEB-INF/views/pay/pay_room.jsp").forward(req, resp);
+		req.getRequestDispatcher("/WEB-INF/views/order/reservation.jsp").forward(req, resp);
 	}
 	
 }
