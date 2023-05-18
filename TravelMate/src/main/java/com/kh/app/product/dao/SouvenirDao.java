533ee83d152cc06eafce67184b59716956b81915
DESKTOP-KR9MMMN\user2 <gtwins2@naver.com>
2023-05-18 18:14:27 +0900

    기념품 조회 및 상세페이지 이동

---
+++ TravelMate/src/main/java/com/kh/app/product/dao/SouvenirDao.java
@@ -0,0 +1,218 @@
+package com.kh.app.product.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.kh.app.common.db.JDBCTemplate;
+import com.kh.app.common.page.PageVo;
+import com.kh.app.product.vo.ProductVo;
+import com.kh.app.product.vo.SouvenirVo;
+
+
+public class SouvenirDao {
+	
+	public List<SouvenirVo> getSouvenirList(Connection conn, PageVo pv) throws Exception {
+		// SQL
+	    String sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND LC.NAME LIKE '%%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+	    PreparedStatement pstmt = conn.prepareStatement(sql);
+	    pstmt.setInt(1, pv.getBeginRow());
+	    pstmt.setInt(2, pv.getLastRow());
+	    ResultSet rs = pstmt.executeQuery();
+	    
+	    // tx || rs
+	    List<SouvenirVo> voList = new ArrayList<>();
+
+	    while (rs.next()) {
+	    	SouvenirVo vo = new SouvenirVo();
+	    	
+	        String no = rs.getString("NO");
+	        String localNo = rs.getString("LOCAL_NO");
+	        String name = rs.getString("NAME");
+	        String enrollDate = rs.getString("ENROLL_DATE");
+	        String content = rs.getString("CONTENT");
+	        String deleteYn = rs.getString("DELETE_YN");
+	        String price = rs.getString("PRICE");
+	        String title = rs.getString("TITLE");
+	        String lcName = rs.getString("LOCAL");
+	        
+	        vo.setNo(no);
+	        vo.setLocalNo(localNo);
+	        vo.setName(name);
+	        vo.setEnrollDate(enrollDate);
+	        vo.setContent(content);
+	        vo.setDeleteYn(deleteYn);
+	        vo.setPrice(price);
+	        vo.setTitle(title);;
+	        vo.setLocal(lcName);
+	        
+	        voList.add(vo);
+	        
+	    }
+
+	    JDBCTemplate.close(rs);
+	    JDBCTemplate.close(pstmt);
+
+	    // 결과가 없는 경우 예외 처리
+	    if (voList.isEmpty()) {
+	        throw new Exception("No data found");
+	    }
+
+	    return voList;
+	}
+	
+	public int getSouvenirListCnt(Connection conn, String searchType, String searchValue) throws Exception {
+		//SQL
+		String sql = "SELECT COUNT(*) FROM ( SELECT * FROM ACCOMODATION) WHERE DELETE_YN = 'N'";
+		if("ACCOMODATION".equals(searchType)) {
+			sql += "AND NAME LIKE '%" + searchValue + "%'";
+		}else if("writer".equals(searchType)) {
+			sql += "AND NICK LIKE '%" + searchValue + "%'";
+		}else if("category".equals(searchType)) {
+			sql += "AND CATEGORY_NO = " + searchValue;
+		}
+		
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		
+		//tx || rs
+		int cnt = 0;
+		if(rs.next()) {
+			cnt = rs.getInt(1);
+		}
+		
+		JDBCTemplate.close(rs);
+		JDBCTemplate.close(pstmt);
+		
+		return cnt;
+	}
+
+	
+
+	public List<SouvenirVo> getSouvenirList(Connection conn, PageVo pv, String searchType, String searchValue, String local) throws Exception {
+		String sql = "";
+		
+		if(searchType.equals("name")) {
+			//SQL (제목	검색)
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(searchType.equals("price")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND LC.NAME LIKE '%'||?||'%' ORDER BY TO_NUMBER(PRICE) ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("gang")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%강원도%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("chung")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%충천남/북도%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("kung")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%경상남/북도%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("jun")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%전라남/북도%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("dae")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%대전%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("ull")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%울산%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("daegu")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%대구%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("gwang")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%광주%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("bu")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%부산%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else if(local.equals("je")) {
+			sql = "SELECT * FROM ( SELECT ROWNUM RNUM , T.* FROM ( SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME LIKE '%'||?||'%' AND LC.NAME LIKE '%제주도%' ORDER BY NO DESC ) T ) WHERE RNUM BETWEEN ? AND ?";
+		}else {
+			//값이 이상함 => 기본 목록 조회
+			return getSouvenirList(conn, pv);
+		}
+		System.out.println(local);
+		System.out.println(searchType);
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString	(1, searchValue);
+		pstmt.setInt	(2, pv.getBeginRow());
+		pstmt.setInt	(3, pv.getLastRow());
+		ResultSet rs = pstmt.executeQuery();
+		
+		//tx || rs
+		List<SouvenirVo> voList = new ArrayList<>();
+		while(rs.next()) {
+			SouvenirVo vo = new SouvenirVo();
+			
+			String no = rs.getString("NO");
+	        String localNo = rs.getString("LOCAL_NO");
+	        String name = rs.getString("NAME");
+	        String enrollDate = rs.getString("ENROLL_DATE");
+	        String content = rs.getString("CONTENT");
+	        String deleteYn = rs.getString("DELETE_YN");
+	        String price = rs.getString("PRICE");
+	        String title = rs.getString("TITLE");
+	        String lcName = rs.getString("LOCAL");
+	        
+	        vo.setNo(no);
+	        vo.setLocalNo(localNo);
+	        vo.setName(name);
+	        vo.setEnrollDate(enrollDate);
+	        vo.setContent(content);
+	        vo.setDeleteYn(deleteYn);
+	        vo.setPrice(price);
+	        vo.setTitle(title);;
+	        vo.setLocal(lcName);
+			
+			
+			voList.add(vo);
+		}
+		System.out.println(voList);
+		JDBCTemplate.close(rs);
+		JDBCTemplate.close(pstmt);
+		
+		return voList;
+	}
+
+	//기념품 상세조회
+	public SouvenirVo selectSouvenirOneByNo(Connection conn, String name) throws Exception {
+		//SQL
+		String sql = "SELECT S.NO, S.LOCAL_NO, S.NAME, S.PRICE, S.ENROLL_DATE, S.DELETE_YN, S.COUNT, S.CONTENT, SI.TITLE, LC.NAME AS LOCAL FROM SOUVENIR S JOIN SOUVENIR_IMG SI ON SI.SOUVENIR_NO = S.NO JOIN LOCAL_CATEGORY LC ON LC.NO = S.LOCAL_NO WHERE S.DELETE_YN = 'N' AND S.NAME = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, name);
+		ResultSet rs = pstmt.executeQuery();
+		//tx || rs
+		SouvenirVo vo = null;
+		if(rs.next()) {
+			vo = new SouvenirVo();
+			
+			String no = rs.getString("NO");
+	        String localNo = rs.getString("LOCAL_NO");
+	        String enrollDate = rs.getString("ENROLL_DATE");
+	        String deleteYn = rs.getString("DELETE_YN");
+			String title = rs.getString("TITLE");
+			String content = rs.getString("CONTENT");
+			String price = rs.getString("PRICE");
+			String lcName = rs.getString("LOCAL");
+			
+			System.out.println(no);
+			
+			vo.setNo(no);
+	        vo.setLocalNo(localNo);
+	        vo.setName(name);
+	        vo.setEnrollDate(enrollDate);
+	        vo.setDeleteYn(deleteYn);
+			vo.setTitle(title);
+			vo.setContent(content);
+			vo.setPrice(price);
+			vo.setLocal(lcName);
+			
+			System.out.println(vo);
+		}
+		
+		//close
+		JDBCTemplate.close(rs);
+		JDBCTemplate.close(pstmt);
+		
+		System.out.println(vo);
+		
+		return vo;
+	}
+	
+
+}
---
+++ TravelMate/src/main/java/com/kh/app/product/service/SouvenirService.java
@@ -0,0 +1,65 @@
+package com.kh.app.product.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.kh.app.common.db.JDBCTemplate;
+import com.kh.app.common.page.PageVo;
+import com.kh.app.product.dao.SouvenirDao;
+import com.kh.app.product.vo.SouvenirVo;
+
+public class SouvenirService {
+	
+	private final SouvenirDao dao = new SouvenirDao();
+	
+	public int getSouvenirListCnt(String searchType, String searchValue) throws Exception {
+		//conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+		int cnt = dao.getSouvenirListCnt(conn , searchType , searchValue);
+		
+		//close
+		JDBCTemplate.close(conn);
+		
+		return cnt;
+	}
+
+
+	public List<SouvenirVo> getSouvenirList(PageVo pv) throws Exception {
+		//conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+		List<SouvenirVo> voList = dao.getSouvenirList(conn , pv);
+		
+		//close
+		JDBCTemplate.close(conn);
+		
+		return voList;
+	}
+
+
+	public List<SouvenirVo> getSouvenirList(PageVo pv, String searchType, String searchValue, String local) throws Exception {
+		//conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+		List<SouvenirVo> voList = dao.getSouvenirList(conn , pv , searchType, searchValue, local);
+		
+		//close
+		JDBCTemplate.close(conn);
+		
+		return voList;
+	}
+
+
+	//기념품 상세페이지
+	public SouvenirVo selectSouvenirOneByNo(String name) throws Exception {
+		SouvenirVo vo = null;
+		//conn
+		try (Connection conn = JDBCTemplate.getConnection();){
+			vo = dao.selectSouvenirOneByNo(conn , name);
+		}
+		return vo;
+	}
+
+}
