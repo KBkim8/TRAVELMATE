a2a6027f9b13c27b66b7ec108f63c0ab10cbcee9
DESKTOP-NN9HF7C\k0322 <k0322123@gmail.com>
2023-05-26 21:43:31 +0900

    헤더 링크 수정, 로그인 로그아웃 시 이동 경로 수정, 주문내역 검색 기능 추가 (UI  구성만 바꾸면 됨.)

--- TravelMate/src/main/java/com/kh/app/member/controller/LoginController.java
+++ TravelMate/src/main/java/com/kh/app/member/controller/LoginController.java
@@ -41,7 +41,7 @@ public class LoginController extends HttpServlet{
 			//wha
 			if(loginMember != null) {
 				req.getSession().setAttribute("loginMember", loginMember);
-				resp.sendRedirect(req.getContextPath()+"/notice/list");
+				resp.sendRedirect(req.getContextPath()+"/home");
 
 			}else{
 				throw new Exception();
--- TravelMate/src/main/java/com/kh/app/mypage/vo/OrderListVo.java
+++ TravelMate/src/main/java/com/kh/app/mypage/vo/OrderListVo.java
@@ -3,269 +3,127 @@ package com.kh.app.mypage.vo;
 public class OrderListVo {
 	
 	private String no;
-	private String accomodationPayNo;
-	private String accomodationPayType;
-	private String accomodationPayDate;
-	private String accomodationImg;
-	private String accomodationName;
-	private String accomodationStartDate;
-	private String accomodationEndDate;
-	private String accomodationPrice;
-	private String accomodationCancelYn;
+	private String payNo;
+	private String payType;
+	private String payDate;
+	private String img;
+	private String name;
+	private String startDate;
+	private String endDate;
+	private String price;
+	private String cancelYn;
 	
-	private String carPayNo;
-	private String carPayType;
-	private String carPayDate;
-	private String carImg;
-	private String carName;
-	private String carStartDate;
-	private String carEndDate;
-	private String carPrice;
-	private String carCancelYn;
-	
-	private String souvenirPayNo;
-	private String souvenirPayType;
-	private String souvenirnPayDate;
-	private String souvenirImg;
-	private String souvenirName;
-	private String souvenirPrice;
-	private String souvenirCancelYn;
-	private String souvenirAddress;
-	private String souvenirOderName;
-	private String souvenirCnt;
-	
-	
-	public String getSouvenirCnt() {
-		return souvenirCnt;
-	}
-	public void setSouvenirCnt(String souvenirCnt) {
-		this.souvenirCnt = souvenirCnt;
-	}
+	private String address;
+	private String oderName;
+	private String cnt;
 	public String getNo() {
 		return no;
 	}
 	public void setNo(String no) {
 		this.no = no;
 	}
-	public String getAccomodationPayNo() {
-		return accomodationPayNo;
+	public String getPayNo() {
+		return payNo;
 	}
-	public void setAccomodationPayNo(String accomodationPayNo) {
-		this.accomodationPayNo = accomodationPayNo;
+	public void setPayNo(String payNo) {
+		this.payNo = payNo;
 	}
-	public String getAccomodationPayType() {
-		return accomodationPayType;
+	public String getPayType() {
+		return payType;
 	}
-	public void setAccomodationPayType(String accomodationPayType) {
-		this.accomodationPayType = accomodationPayType;
+	public void setPayType(String payType) {
+		this.payType = payType;
 	}
-	public String getAccomodationPayDate() {
-		return accomodationPayDate;
+	public String getPayDate() {
+		return payDate;
 	}
-	public void setAccomodationPayDate(String accomodationPayDate) {
-		this.accomodationPayDate = accomodationPayDate;
+	public void setPayDate(String payDate) {
+		this.payDate = payDate;
 	}
-	public String getAccomodationImg() {
-		return accomodationImg;
+	public String getImg() {
+		return img;
 	}
-	public void setAccomodationImg(String accomodationImg) {
-		this.accomodationImg = accomodationImg;
+	public void setImg(String img) {
+		this.img = img;
 	}
-	public String getAccomodationName() {
-		return accomodationName;
+	public String getName() {
+		return name;
 	}
-	public void setAccomodationName(String accomodationName) {
-		this.accomodationName = accomodationName;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public String getAccomodationStartDate() {
-		return accomodationStartDate;
+	public String getStartDate() {
+		return startDate;
 	}
-	public void setAccomodationStartDate(String accomodationStartDate) {
-		this.accomodationStartDate = accomodationStartDate;
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
 	}
-	public String getAccomodationEndDate() {
-		return accomodationEndDate;
+	public String getEndDate() {
+		return endDate;
 	}
-	public void setAccomodationEndDate(String accomodationEndDate) {
-		this.accomodationEndDate = accomodationEndDate;
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
 	}
-	public String getAccomodationPrice() {
-		return accomodationPrice;
+	public String getPrice() {
+		return price;
 	}
-	public void setAccomodationPrice(String accomodationPrice) {
-		this.accomodationPrice = accomodationPrice;
+	public void setPrice(String price) {
+		this.price = price;
 	}
-	public String getAccomodationCancelYn() {
-		return accomodationCancelYn;
+	public String getCancelYn() {
+		return cancelYn;
 	}
-	public void setAccomodationCancelYn(String accomodationCancelYn) {
-		this.accomodationCancelYn = accomodationCancelYn;
+	public void setCancelYn(String cancelYn) {
+		this.cancelYn = cancelYn;
 	}
-	public String getCarPayNo() {
-		return carPayNo;
+	public String getAddress() {
+		return address;
 	}
-	public void setCarPayNo(String carPayNo) {
-		this.carPayNo = carPayNo;
+	public void setAddress(String address) {
+		this.address = address;
 	}
-	public String getCarPayType() {
-		return carPayType;
+	public String getOderName() {
+		return oderName;
 	}
-	public void setCarPayType(String carPayType) {
-		this.carPayType = carPayType;
+	public void setOderName(String oderName) {
+		this.oderName = oderName;
 	}
-	public String getCarPayDate() {
-		return carPayDate;
+	public String getCnt() {
+		return cnt;
 	}
-	public void setCarPayDate(String carPayDate) {
-		this.carPayDate = carPayDate;
-	}
-	public String getCarImg() {
-		return carImg;
-	}
-	public void setCarImg(String carImg) {
-		this.carImg = carImg;
-	}
-	public String getCarName() {
-		return carName;
-	}
-	public void setCarName(String carName) {
-		this.carName = carName;
-	}
-	public String getCarStartDate() {
-		return carStartDate;
-	}
-	public void setCarStartDate(String carStartDate) {
-		this.carStartDate = carStartDate;
-	}
-	public String getCarEndDate() {
-		return carEndDate;
-	}
-	public void setCarEndDate(String carEndDate) {
-		this.carEndDate = carEndDate;
-	}
-	public String getCarPrice() {
-		return carPrice;
-	}
-	public void setCarPrice(String carPrice) {
-		this.carPrice = carPrice;
-	}
-	public String getCarCancelYn() {
-		return carCancelYn;
-	}
-	public void setCarCancelYn(String carCancelYn) {
-		this.carCancelYn = carCancelYn;
-	}
-	public String getSouvenirPayNo() {
-		return souvenirPayNo;
-	}
-	public void setSouvenirPayNo(String souvenirPayNo) {
-		this.souvenirPayNo = souvenirPayNo;
-	}
-	public String getSouvenirPayType() {
-		return souvenirPayType;
-	}
-	public void setSouvenirPayType(String souvenirPayType) {
-		this.souvenirPayType = souvenirPayType;
-	}
-	public String getSouvenirnPayDate() {
-		return souvenirnPayDate;
-	}
-	public void setSouvenirnPayDate(String souvenirnPayDate) {
-		this.souvenirnPayDate = souvenirnPayDate;
-	}
-	public String getSouvenirImg() {
-		return souvenirImg;
-	}
-	public void setSouvenirImg(String souvenirImg) {
-		this.souvenirImg = souvenirImg;
-	}
-	public String getSouvenirName() {
-		return souvenirName;
-	}
-	public void setSouvenirName(String souvenirName) {
-		this.souvenirName = souvenirName;
-	}
-	public String getSouvenirPrice() {
-		return souvenirPrice;
-	}
-	public void setSouvenirPrice(String souvenirPrice) {
-		this.souvenirPrice = souvenirPrice;
-	}
-	public String getSouvenirCancelYn() {
-		return souvenirCancelYn;
-	}
-	public void setSouvenirCancelYn(String souvenirCancelYn) {
-		this.souvenirCancelYn = souvenirCancelYn;
-	}
-	public String getSouvenirAddress() {
-		return souvenirAddress;
-	}
-	public void setSouvenirAddress(String souvenirAddress) {
-		this.souvenirAddress = souvenirAddress;
-	}
-	public String getSouvenirOderName() {
-		return souvenirOderName;
-	}
-	public void setSouvenirOderName(String souvenirOderName) {
-		this.souvenirOderName = souvenirOderName;
+	public void setCnt(String cnt) {
+		this.cnt = cnt;
 	}
 	@Override
 	public String toString() {
-		return "OrderListVo [no=" + no + ", accomodationPayNo=" + accomodationPayNo + ", accomodationPayType="
-				+ accomodationPayType + ", accomodationPayDate=" + accomodationPayDate + ", accomodationImg="
-				+ accomodationImg + ", accomodationName=" + accomodationName + ", accomodationStartDate="
-				+ accomodationStartDate + ", accomodationEndDate=" + accomodationEndDate + ", accomodationPrice="
-				+ accomodationPrice + ", accomodationCancelYn=" + accomodationCancelYn + ", carPayNo=" + carPayNo
-				+ ", carPayType=" + carPayType + ", carPayDate=" + carPayDate + ", carImg=" + carImg + ", carName="
-				+ carName + ", carStartDate=" + carStartDate + ", carEndDate=" + carEndDate + ", carPrice=" + carPrice
-				+ ", carCancelYn=" + carCancelYn + ", souvenirPayNo=" + souvenirPayNo + ", souvenirPayType="
-				+ souvenirPayType + ", souvenirnPayDate=" + souvenirnPayDate + ", souvenirImg=" + souvenirImg
-				+ ", souvenirName=" + souvenirName + ", souvenirPrice=" + souvenirPrice + ", souvenirCancelYn="
-				+ souvenirCancelYn + ", souvenirAddress=" + souvenirAddress + ", souvenirOderName=" + souvenirOderName
-				+ "]";
+		return "OrderListVo [no=" + no + ", payNo=" + payNo + ", payType=" + payType + ", payDate=" + payDate + ", img="
+				+ img + ", name=" + name + ", startDate=" + startDate + ", endDate=" + endDate + ", price=" + price
+				+ ", cancelYn=" + cancelYn + ", address=" + address + ", oderName=" + oderName + ", cnt=" + cnt + "]";
 	}
-	public OrderListVo(String no, String accomodationPayNo, String accomodationPayType, String accomodationPayDate,
-			String accomodationImg, String accomodationName, String accomodationStartDate, String accomodationEndDate,
-			String accomodationPrice, String accomodationCancelYn, String carPayNo, String carPayType,
-			String carPayDate, String carImg, String carName, String carStartDate, String carEndDate, String carPrice,
-			String carCancelYn, String souvenirPayNo, String souvenirPayType, String souvenirnPayDate,
-			String souvenirImg, String souvenirName, String souvenirPrice, String souvenirCancelYn,
-			String souvenirAddress, String souvenirOderName) {
+	public OrderListVo(String no, String payNo, String payType, String payDate, String img, String name,
+			String startDate, String endDate, String price, String cancelYn, String address, String oderName,
+			String cnt) {
 		super();
 		this.no = no;
-		this.accomodationPayNo = accomodationPayNo;
-		this.accomodationPayType = accomodationPayType;
-		this.accomodationPayDate = accomodationPayDate;
-		this.accomodationImg = accomodationImg;
-		this.accomodationName = accomodationName;
-		this.accomodationStartDate = accomodationStartDate;
-		this.accomodationEndDate = accomodationEndDate;
-		this.accomodationPrice = accomodationPrice;
-		this.accomodationCancelYn = accomodationCancelYn;
-		this.carPayNo = carPayNo;
-		this.carPayType = carPayType;
-		this.carPayDate = carPayDate;
-		this.carImg = carImg;
-		this.carName = carName;
-		this.carStartDate = carStartDate;
-		this.carEndDate = carEndDate;
-		this.carPrice = carPrice;
-		this.carCancelYn = carCancelYn;
-		this.souvenirPayNo = souvenirPayNo;
-		this.souvenirPayType = souvenirPayType;
-		this.souvenirnPayDate = souvenirnPayDate;
-		this.souvenirImg = souvenirImg;
-		this.souvenirName = souvenirName;
-		this.souvenirPrice = souvenirPrice;
-		this.souvenirCancelYn = souvenirCancelYn;
-		this.souvenirAddress = souvenirAddress;
-		this.souvenirOderName = souvenirOderName;
+		this.payNo = payNo;
+		this.payType = payType;
+		this.payDate = payDate;
+		this.img = img;
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.price = price;
+		this.cancelYn = cancelYn;
+		this.address = address;
+		this.oderName = oderName;
+		this.cnt = cnt;
 	}
 	public OrderListVo() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	
+
 	
 
 }
